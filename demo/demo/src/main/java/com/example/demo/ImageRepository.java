793a1a0f70396f1e47dff7b4412ac3c7577685ac
AkashDeolikar <125456176+AkashDeolikar@users.noreply.github.com>
2025-03-11 21:14:34 +0530

    Delete demo/demo directory

--- demo/demo/src/main/java/com/example/demo/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo;
-
-import com.example.demo.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
